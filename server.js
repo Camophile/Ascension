0abd35d334bbbcc6b5eee06d1d152021da2c85ff
Arman Kazemi <kas1er@hotmail.com>
2018-09-22 00:29:19 -0700

    migrating files to handlebars so we can start viewing things in partials and and create multi-path layouts

--- server.js
+++ server.js
@@ -2,11 +2,40 @@
  * Module dependencies.
  */
 const express = require('express');
+const hbs = require('express-handlebars');
 const path = require('path');
+
 const homePage = require('./public/app');
 const router = express.Router();
 
 const app = express();
+
+/*
+   * view engine config
+   */
+  app.engine('hbs', hbs({
+    // set file extenstions to .hbs
+    extname: 'hbs',
+    helpers: {
+      if_eq: function (a, b, opts) {
+        if (a == b) {
+          return opts.fn(this);
+        }
+        return opts.inverse(this);
+      },
+    },
+    // set defaultLayout
+    defaultLayout: 'main',
+    // set defaultLayout directory
+    layoutsDir: `${__dirname}/public/views/layouts/`,
+    // set partial directory
+    partialsDir: `${__dirname}/public/views/partials`,
+  }));
+
+  app.set('views', `${__dirname}/public`);
+  app.set('view engine', 'hbs');
+  
+  app.use(express.static(`${__dirname}/public`));
 
 const publicDir = `${__dirname}/public`;
 
@@ -19,9 +48,12 @@ const publicDir = `${__dirname}/public`;
   outputStyle: 'expanded',
 })); */
 
-app.use(express.static(`${publicDir}/construction`));
+//app.use(express.static(`${publicDir}/construction`));
 
-app.use('/no-view-mode', homePage);
+app.get('/', (req, res) => {
+  res.render('home');
+});
+//app.use('/no-view-mode', homePage);
 /* app.use('/', express.static(path.join(__dirname, '/public/construction'),
   {
     extensions:['html'],
